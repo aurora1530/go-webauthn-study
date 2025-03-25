e46e58d5a1b8a6f956fc8152bb0813ac77a1c96a
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-25 17:21:42 +0900

    add(backend): implement database connection in server initialization

--- backend/internal/server.go
+++ backend/internal/server.go
@@ -1,9 +1,36 @@
 package internal
 
-type Server struct {
+import (
+	"database/sql"
+	"fmt"
+	"os"
 
+	_ "github.com/lib/pq"
+)
+
+type Server struct {
+	*sql.DB
+}
+
+func openDB() *sql.DB {
+	user := os.Getenv("POSTGRES_USER")
+	pass := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	host := "db"
+	port := "5432"
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, pass, host, port, dbName)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database")
+	return db
 }
 
 func NewServer() Server{
-	return Server{}
+	db := openDB()
+	return Server{db}
 }
